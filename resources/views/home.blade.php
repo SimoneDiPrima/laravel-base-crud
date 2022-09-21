348314a230c03bc5d4996412cd29f626bb8f6384
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-21 16:33:58 +0200

    creazione main.blade Model Comic e HomeController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.main');
+@section('main-content')
+<h4 class="text-center text-success">Mamma guarda come sto shakerando</h4>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h4 class="text-warning text-center">COMICS LIST</h4>
+        <div class="col-3 card mt-5">
+            @yield('main-content')
+        </div>
+    </div>
+   
+</body>
+</html>
