6275cbeed7fd9d57be8f01e490a0a25ff8ee1e1f
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-22 15:17:04 +0200

    utilizzo resource

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('home');
-Route::get('/comics',function(){
-    return view('comics.index');
-})->name('comics.index');
+Route::resource('Comics','ComicController');
