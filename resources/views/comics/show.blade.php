6096fe9ad70105fdc2bc106955a6341cffa0d82c
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-23 19:01:47 +0200

    modifiche homepage con aggiunta bottone e modifiche stile

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.main');
 @section('main-content')
-<h2>{{ $comic->title }}</h2>
-<h2>{{ $comic->description }}</h2>
-<h2>{{ $comic->thumb }}</h2>
-<h2>{{ $comic->price }}</h2>
-<h2>{{ $comic->series }}</h2>
-<h2>{{ $comic->sale_date }}</h2>
-<h2>{{ $comic->type }}</h2>
+<h2 class="mb-4">{{ $comic->title }}</h2>
+<h5 class="mb-4">{{ $comic->description }}</h2>
+<img class="mb-4" src="{{ $comic->thumb }}" alt="poster" />
+<h3 class="mb-4">{{ $comic->price }} €</h2>
+<h5 class="mb-4">{{ $comic->series }}</h2>
+<h5 class="mb-4">{{ $comic->sale_date }}</h2>
+<h5 class="mb-4">{{ $comic->type }}</h2>
+<a class="offset-10 col-2" href="{{ route('home') }}"><button class="btn btn-primary mb-5">vedi nell archivio</button></a>
 
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,21 @@
 @extends('layouts.main');
 @section('main-content')
-<div class="homepage">
-    <h4 class="text-center text-success">Comic's world</h4>
+<div class="homepage bg-dark my-5 py-5">
+    <h4 class="text-center text-success mt-4">Comic's world</h4>
     <h6 class="text-primary text-center">entra nel nostro fantastico mondo e scegli la collezione piu adatta a te</h6>
-    <button type="submit" class="offset-4 col-4 bg-primary text-white my-5 rounded-pill border-danger">CLICCA QUI</button>
+    <a href="{{ route('Comics.create') }}"><button type="submit" class="offset-4 col-4 bg-primary text-white my-5 p-2 rounded-pill border-danger">AGGIUNGI IL TUO PROSSIMO TOPSELLER</button></a>
+    <div class="d-flex flex-wrap justify-content-center">
+@foreach($comics as $index=>$comic)
+<a class="p-1" href="">   
+        <h6 class="text-white w-80">{{$comic['title']}}</h6>
+        <img src="{{$comic['thumb']}}" alt=""/>
+    
+</a>
+
+@endforeach
+
 </div>
+</div>
+
 
 @endsection
