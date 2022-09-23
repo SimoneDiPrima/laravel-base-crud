659f06d4bdb8c98985924fa407fb1f94915d33b6
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-23 18:00:37 +0200

    modifiche homepage e home.blade

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,3 +1,6 @@
 @extends('layouts.main');
 @section('main-content')
+dump($comics)
+<h1>COMIC'S WORLD</h1>
+<h3>ARCHIVE</h3>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.main');
 @section('main-content')
-<h4 class="text-center text-success"></h4>
+<div class="homepage">
+    <h4 class="text-center text-success">Comic's world</h4>
+    <h6 class="text-primary text-center">entra nel nostro fantastico mondo e scegli la collezione piu adatta a te</h6>
+    <button type="submit" class="offset-4 col-4 bg-primary text-white my-5 rounded-pill border-danger">CLICCA QUI</button>
+</div>
+
 @endsection
